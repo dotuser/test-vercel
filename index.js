0d058d88fd80e7574b1bccec0d5ccbd0ffa6206e
dotuser <ahmedhassaan@tutanota.com>
2024-10-19 11:33:26 +0500

    Home End Point Added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
+app.get("/home", (req, res) => res.send("Home"));
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
