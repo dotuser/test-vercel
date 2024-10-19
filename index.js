6d6981f28217366322130fea279cba5d8a9860e7
dotuser <ahmedhassaan@tutanota.com>
2024-10-19 11:38:50 +0500

    Visitor End Point Added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const visitorRoutes = require('./routes/visitors');
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
 app.get("/home", (req, res) => res.send("Home"));
+
+app.use("/api/visitors", visitorRoutes);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
