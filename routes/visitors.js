c7cc2f0eef6acffad52f8783c2b2ab1a3f51ce71
dotuser <ahmedhassaan@tutanota.com>
2024-10-19 11:58:21 +0500

    Visitors | Add & Get All Req Created

--- routes/visitors.js
+++ routes/visitors.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const Visitor = require('../models/Visitor');
 const router = express.Router();
 
-router.get('/', (req, res) => res.send("I'm a visitor"));
+router.post('/', async (req, res) => {
+    try {
+        const visitor = new Visitor({
+            ...req.body,
+        });
+        const savedVisitor = await visitor.save();
+        res.status(201).send(savedVisitor);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const visitors = await Visitor.find().select('-__v');
+        res.send(visitors);
+    } catch (error) {
+        res.send({ message: error.message });
+    }
+});
 
 module.exports = router;
