b0009ba55b87bfa6ed82a99664ec8b4729a61f79
dotuser <ahmedhassaan@tutanota.com>
2024-10-23 11:26:49 +0500

    User Helper Functions

--- helper/UserHelper.js
+++ helper/UserHelper.js
@@ -1,12 +1,60 @@
 const User = require('../models/Users');
 
-const findUserByCnic = async cnic => {
+//  EMAIL
+const isValidEmail = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+
+const doesEmailExist = async email => {
   try {
-    const user = await User.findOne({ cnic });
-    console.log(user);
+    if (isValidEmail(email)) {
+      const user = await User.findOne({ email });
+      return !!user;
+    }
+    return false;
   } catch (err) {
     console.error(err);
+    return false;
   }
-}
+};
 
-module.exports = { findUserByCnic };
+
+//  MOBILE #
+const isValidMobileNo = contact => {
+  return /^[0-9]{11}$/.test(contact); 
+};
+
+const doesContactNoExist = async mobileNo => {
+  try {
+    const isValidContactNo = isValidMobileNo(mobileNo);
+    if (isValidContactNo) {
+      const user = await User.findOne({ mobileNo });
+      return !!user;
+    }
+    return false;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+//  CNIC
+const isValidCnic = cnic => {
+  return typeof cnic === 'string' && /^[0-9]{13}$/.test(cnic);
+};
+
+const doesCnicExist = async cnic => {
+  try {
+    const isValidNic = isValidCnic(cnic);
+    if (isValidNic) {
+      const user = await User.findOne({ cnic });
+      return !!user;
+    }
+    return false;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+};
+
+module.exports = { doesCnicExist, isValidCnic, isValidMobileNo, doesContactNoExist, isValidEmail, doesEmailExist };
