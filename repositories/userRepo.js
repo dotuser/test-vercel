72c7973cf7e077ada3878b15b2da78d95689181f
dotuser <ahmedhassaan@tutanota.com>
2024-10-26 15:14:58 +0500

    Upload & Download CV Func | Unnecessary Code Removed

--- repositories/userRepo.js
+++ repositories/userRepo.js
@@ -1,5 +1,7 @@
 const User = require('../models/Users');
 const { doesCnicExist, doesEmailExist, doesContactNoExist, isValidCnic, isValidMobileNo, isValidEmail } = require('../helper/UserHelper');
+const path = require("path");
+const fs = require("fs");
 
 // Create User
 const createUser = async (req, res) => {
@@ -13,18 +15,10 @@ const createUser = async (req, res) => {
     const emailExists = await doesEmailExist(email);
     const mobileNoExists = await doesContactNoExist(mobileNo);
 
-    if (cnicRegistered) {
-      return res.status(400).json({ message: 'CNIC is already Registered'});
-    }
-
-    if (emailExists) {
-      return res.status(400).json({ message: 'Email is already Registered'});
-    }
-
-    if (mobileNoExists) {
-      return res.status(400).json({ message: 'Mobile Number is already Registered'});
-    }
-    
+    if (cnicRegistered) return res.status(400).json({ message: 'CNIC is already Registered'});
+    if (emailExists) return res.status(400).json({ message: 'Email is already Registered'});
+    if (mobileNoExists) return res.status(400).json({ message: 'Mobile Number is already Registered'});
+  
     const user = new User({
       ...req.body,
       userTypeId: userTypeId,
@@ -41,8 +35,6 @@ const createUser = async (req, res) => {
 // Get All Users
 const getAllUsers = async (req, res) => {
   try {
-    console.log('I am a USER');
-    
     const userTypeId = req.params.userTypeId;
     const users = await User.find({ isActive: true, userTypeId: userTypeId });
     res.status(200).json(users);
@@ -56,9 +48,8 @@ const getUserById = async (req, res) => {
   try {
     const userId = req.params.id;
     const user = await User.findOne({ _id: userId, isActive: true });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -67,58 +58,79 @@ const getUserById = async (req, res) => {
 
 // Update User
 const updateUser = async (req, res) => {
+  const userId = req.params.id;
+  const { cnic, email, mobileNo } = req.body;
+
   try {
-    // const userTypeId = req.params.userTypeId;
-    const userId = req.params.id;
-    const cnic = req.body.cnic;
-    const email = req.body.email;
-    const mobileNo = req.body.mobileNo;
+    const user = await User.findOne({ _id: userId, isActive: true });
+    if (!user) return res.status(404).json({ message: 'User not found or not active' });
 
-    const validateCnic = isValidCnic(cnic);
-    const validateEmail = isValidEmail(email);
-    const validateMobileNo = isValidMobileNo(mobileNo);
+    if (cnic && !isValidCnic(cnic)) return res.status(400).json({ message: 'Invalid CNIC' });
+    if (email && !isValidEmail(email)) return res.status(400).json({ message: 'Invalid Email Address' });
+    if (mobileNo && !isValidMobileNo(mobileNo)) return res.status(400).json({ message: 'Invalid Mobile Number' });
 
-    if (cnic && !validateCnic) {
-      return res.status(400).json({ message: 'Invalid CNIC' });
-    }
+    const [cnicRegistered, emailExists, mobileNoExists] = await Promise.all([
+      cnic ? doesCnicExist(cnic) : false,
+      email ? doesEmailExist(email) : false,
+      mobileNo ? doesContactNoExist(mobileNo) : false,
+    ]);
 
-    if (email && !validateEmail) {
-      return res.status(400).json({ message: 'Invalid CNIC' });
-    }
+    if (cnic && cnicRegistered) return res.status(400).json({ message: 'CNIC is already registered' });
+    if (email && emailExists) return res.status(400).json({ message: 'Email is already registered' });
+    if (mobileNo && mobileNoExists) return res.status(400).json({ message: 'Mobile Number is already registered' });
 
-    if (mobileNo && !validateMobileNo) {
-      return res.status(400).json({ message: 'Invalid CNIC' });
-    }
-
-    const cnicRegistered = cnic && await doesCnicExist(cnic);
-    const emailExists = email && await doesEmailExist(email);
-    const mobileNoExists = mobileNo && await doesContactNoExist(mobileNo);
-
-    if (cnic && cnicRegistered) {
-      return res.status(400).json({ message: 'CNIC is already Registered'});
-    }
-
-    if (email && emailExists) {
-      return res.status(400).json({ message: 'Email is already Registered'});
-    }
-
-    if (mobileNo && mobileNoExists) {
-      return res.status(400).json({ message: 'Mobile Number is already Registered'});
-    }
-    
-    const user = await User.findOneAndUpdate(
+    const updatedUser = await User.findOneAndUpdate(
       { _id: userId, isActive: true },
-      { $set: { ...req.body } },
+      { $set: req.body },
       { new: true }
     );
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.status(200).json(user);
+
+    return res.status(200).json(updatedUser);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    console.error(error);
+    return res.status(500).json({ message: 'An error occurred while updating the user' });
   }
 };
+
+
+// const updateUser = async (req, res) => {
+//   const userId = req.params.id;
+//   const cnic = req.body.cnic;
+//   const email = req.body.email;
+//   const mobileNo = req.body.mobileNo;
+
+//   try {
+//     const user = User.findById(userId);
+
+//     if (!user) return res.status(404).json({ message: 'User not found' });
+//     if (!user.isActive) return res.status(400).json({ message: 'Bad Request' });
+
+//     const validateCnic = isValidCnic(cnic);
+//     const validateEmail = isValidEmail(email);
+//     const validateMobileNo = isValidMobileNo(mobileNo);
+
+//     if (cnic && !validateCnic) return res.status(400).json({ message: 'Invalid CNIC' });
+//     if (email && !validateEmail) return res.status(400).json({ message: 'Invalid Email Address' });
+//     if (mobileNo && !validateMobileNo) return res.status(400).json({ message: 'Invalid Mobile Number' });
+
+//     const cnicRegistered = cnic && await doesCnicExist(cnic);
+//     const emailExists = email && await doesEmailExist(email);
+//     const mobileNoExists = mobileNo && await doesContactNoExist(mobileNo);
+
+//     if (cnic && cnicRegistered) return res.status(400).json({ message: 'CNIC is already Registered'});
+//     if (email && emailExists) return res.status(400).json({ message: 'Email is already Registered'});
+//     if (mobileNo && mobileNoExists) return res.status(400).json({ message: 'Mobile Number is already Registered'});
+  
+//     const user = await User.findOneAndUpdate(
+//       { _id: userId, isActive: true },
+//       { $set: { ...req.body } },
+//       { new: true }
+//     );
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
 
 // Delete (Deactivate) User
 const deleteUser = async (req, res) => {
@@ -129,10 +141,53 @@ const deleteUser = async (req, res) => {
       { $set: { isActive: false } },
       { new: true }
     );
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
+    if (!user) return res.status(404).json({ message: 'User not found' })
     res.status(200).json({ message: 'User deactivated successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const uploadCV = async (req, res) => {
+  if (!req.file) return res.status(400).send('No file uploaded.');
+  
+  const userId = req.params.userId;
+  const fileName = req.file.filename;
+  
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: userId, isActive: true },
+      { $set: { 'professionalDetails.cv': fileName }},
+      { new: true }
+    );
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json({ message: 'File uploaded successfully' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const downloadCV = async (req, res) => {
+  try {
+    const { userId, adminId } = req.body;
+
+    if (adminId) {
+      const admin = await User.findById({ _id: adminId, userTypeId: 1, isActive: true });
+      if (!admin) return res.status(404).json({ message: 'Admin User not found' });
+    }
+
+    const user = await User.findById({ _id: userId, isActive: true });
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    const filePath = path.resolve(__dirname, '../uploads', user.professionalDetails.cv);
+    console.log('File Path:', filePath);
+
+    if (!fs.existsSync(filePath)) return res.status(404).send('File not found');
+
+    res.download(filePath, (err) => {
+      if (err) return res.status(500).send('Error downloading file.');
+    });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -144,4 +199,6 @@ module.exports = {
   getUserById,
   updateUser,
   deleteUser,
+  uploadCV,
+  downloadCV
 };
