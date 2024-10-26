654cf69071a7c3d22af7b75e856db626e8cec6a8
dotuser <ahmedhassaan@tutanota.com>
2024-10-26 15:14:15 +0500

    File (CV) Upload | Multer Config

--- config/multer.js
+++ config/multer.js
@@ -1,10 +1,30 @@
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadDir = 'uploads';
+try {
+  if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir);
+} catch (error) {
+  console.error("Error creating uploads directory:", error);
+}
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'application/pdf') cb(null, true);
+  else cb(new Error('Not a PDF file!'), false);
+};
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, uploadDir);
   },
+
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
+    const id = req.params.userId;
+    const fileName = id + '-' + Date.now() + path.extname(file.originalname);
+    cb(null, fileName);
+  },
 });
 
-module.exports = storage;
+const upload = multer({ storage, fileFilter });
+module.exports = { upload };
