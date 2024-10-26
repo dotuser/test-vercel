b0eb02f78c3df8f5a6c2ef3fea5fd38b9c025f44
dotuser <ahmedhassaan@tutanota.com>
2024-10-26 15:15:30 +0500

    Upload & Download CV Routes Added | User Routes

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const { verifyToken, authenticate } = require('../config/jwt');
+const { upload } = require('../config/multer');
 const { createUser,
   getAllUsers,
   getUserById,
   updateUser,
-  deleteUser
+  deleteUser,
+  uploadCV,
+  downloadCV
 } = require('../repositories/userRepo');
 
 const router = express.Router();
 
-router.post('/user/:userTypeId/', authenticate, createUser);
-router.get('/users/:userTypeId/', verifyToken, getAllUsers);
+router.post('/user/:userTypeId', authenticate, createUser);
+router.get('/users/:userTypeId', verifyToken, getAllUsers);
 router.get('/user/:id', verifyToken, getUserById);
 router.put('/user:id', verifyToken, updateUser);
 router.delete('/user/:id', verifyToken, deleteUser);
+
+//  CV
+router.post('/user/cv/upload/:userId/:userTypeId', authenticate, upload.single('pdf'), uploadCV);
+router.post('/user/cv/download', verifyToken, downloadCV);
 
 module.exports = router;
